e073365b93d6e19a0c6e39982d1874e870836aaa
Dan <hyounguk1112@gmail.com>
2018-07-17 17:28:07 +0900

    update

--- gan_eval.py
+++ gan_eval.py
@@ -28,24 +28,93 @@ batch_size = 128
 # 	gan_model.train(result_dir, ckpt_dir, log_dir, training_iteration = 5005, G_update_ratio=5, D_update_ratio=1, Q_update_ratio=1)
 # 	del gan_data
 # 	del gan_model
-for i in range(1, 11):
-	for j in range(1, 4):
-		result_dir = os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', 'weight_'+str(i)+'_learning_rate_'+str(j), 'result')
-		ckpt_dir =  os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', 'weight_'+str(i)+'_learning_rate_'+str(j), 'weight')
-		log_dir =  os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', 'weight_'+str(i)+'_learning_rate_'+str(j), 'weight')
-		if not os.path.isdir(result_dir):
-			os.makedirs(result_dir)
-		if not os.path.isdir(ckpt_dir):
-			os.makedirs(ckpt_dir)
-		if not os.path.isdir(log_dir):
-			os.makedirs(log_dir)
-		gan_data = data.Data(cat_dim, code_con_dim, 30, channel, path, name, split_name, batch_size)
-		gan_data.mutual_penalty_weight = i
-		gan_data.learning_rate = 10**j
-		gan_model = infogan.Info_gan(gan_data)
-		gan_model.train(result_dir, ckpt_dir, log_dir, training_iteration = 50005, G_update_num=5, D_update_num=1, Q_update_num=1)
-		del gan_data
-		del gan_model
+
+gan_data = data.Data(cat_dim, code_con_dim, 10, channel, path, name, split_name, batch_size)
+gan_data.mutual_penalty_weight = 3
+gan_data.learning_rate = 10**2
+gan_model = infogan.Info_gan(gan_data)
+result_dir = os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', '101', 'result')
+ckpt_dir =  os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', '101', 'weight')
+log_dir =  os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', '101', 'weight')
+if not os.path.isdir(result_dir):
+		os.makedirs(result_dir)
+if not os.path.isdir(ckpt_dir):
+		os.makedirs(ckpt_dir)
+if not os.path.isdir(log_dir):
+		os.makedirs(log_dir)
+gan_model.train(result_dir, ckpt_dir, log_dir, training_iteration = 50005, G_update_num=5, D_update_num=1, Q_update_num=1)
+del gan_data
+del gan_model
+
+gan_data = data.Data(cat_dim, code_con_dim, 20, channel, path, name, split_name, batch_size)
+gan_data.mutual_penalty_weight = 3
+gan_data.learning_rate = 10**2
+gan_model = infogan.Info_gan(gan_data)
+result_dir = os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', '102', 'result')
+ckpt_dir =  os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', '102', 'weight')
+log_dir =  os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', '102', 'weight')
+if not os.path.isdir(result_dir):
+		os.makedirs(result_dir)
+if not os.path.isdir(ckpt_dir):
+		os.makedirs(ckpt_dir)
+if not os.path.isdir(log_dir):
+		os.makedirs(log_dir)
+gan_model.train(result_dir, ckpt_dir, log_dir, training_iteration = 50005, G_update_num=5, D_update_num=1, Q_update_num=1)
+del gan_data
+del gan_model
+
+gan_data = data.Data(cat_dim, code_con_dim, 30, channel, path, name, split_name, batch_size)
+gan_data.mutual_penalty_weight = 3
+gan_data.learning_rate = 10**2
+gan_model = infogan.Info_gan(gan_data)
+result_dir = os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', '103', 'result')
+ckpt_dir =  os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', '103', 'weight')
+log_dir =  os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', '103', 'weight')
+if not os.path.isdir(result_dir):
+		os.makedirs(result_dir)
+if not os.path.isdir(ckpt_dir):
+		os.makedirs(ckpt_dir)
+if not os.path.isdir(log_dir):
+		os.makedirs(log_dir)
+gan_model.train(result_dir, ckpt_dir, log_dir, training_iteration = 50005, G_update_num=5, D_update_num=1, Q_update_num=1)
+del gan_data
+del gan_model
+
+i=2
+j = 2
+result_dir = os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', 'weight_'+str(i)+'_learning_rate_'+str(j), 'result')
+ckpt_dir =  os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', 'weight_'+str(i)+'_learning_rate_'+str(j), 'weight')
+log_dir =  os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', 'weight_'+str(i)+'_learning_rate_'+str(j), 'weight')
+
+gan_data = data.Data(cat_dim, code_con_dim, 30, channel, path, name, split_name, batch_size)
+gan_data.mutual_penalty_weight = i
+gan_data.learning_rate = 10**j
+gan_model = infogan.Info_gan(gan_data)
+gan_model.train(result_dir, ckpt_dir, log_dir, training_iteration = 150005, G_update_num=5, D_update_num=1, Q_update_num=2)
+del gan_data
+del gan_model
+
+
+# for i in range(1, 8):
+# 	# for j in range(1, 4):
+# 		j = 3
+# 		result_dir = os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', 'weight_'+str(i)+'_learning_rate_'+str(j), 'result')
+# 		ckpt_dir =  os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', 'weight_'+str(i)+'_learning_rate_'+str(j), 'weight')
+# 		log_dir =  os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', 'weight_'+str(i)+'_learning_rate_'+str(j), 'weight')
+# 		if not os.path.isdir(result_dir):
+# 			os.makedirs(result_dir)
+# 		if not os.path.isdir(ckpt_dir):
+# 			os.makedirs(ckpt_dir)
+# 		if not os.path.isdir(log_dir):
+# 			os.makedirs(log_dir)
+# 		gan_data = data.Data(cat_dim, code_con_dim, 30, channel, path, name, split_name, batch_size)
+# 		gan_data.mutual_penalty_weight = i
+# 		gan_data.learning_rate = 10**j
+# 		gan_model = infogan.Info_gan(gan_data)
+# 		gan_model.train(result_dir, ckpt_dir, log_dir, training_iteration = 100005, G_update_num=5, D_update_num=1, Q_update_num=2)
+# 		del gan_data
+# 		del gan_model
+
 
 # for i in range(1, 4):
 # 	result_dir = os.path.join('/media/dan/DATA/hukim/Research/srnet/infogan_exp', 'con_loss_x2', 'result')
@@ -64,3 +133,4 @@ for i in range(1, 11):
 # 	#gan_model.train(result_dir, ckpt_dir, log_dir, training_iteration = 50005, G_update_num=5, D_update_num=1, Q_update_num=3)
 # 	del gan_data
 # 	del gan_model
+
